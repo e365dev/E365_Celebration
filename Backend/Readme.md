fec6b20b1f219166b607ceae101573b09f6719dc
Premjit Das <dpremjit42@gmail.com>
2025-12-29 12:15:43 +0530

    Initial commit

---
+++ Backend/Readme.md
@@ -0,0 +1,1 @@
+# Backend of E365 Celebration
---
+++ Frontend/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+      <h1 className="text-5xl font-bold text-gray-800">
+        Hello
+      </h1>
+    </div>
+  );
+}
+
+export default App;
